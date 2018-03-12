654553e6e7f4afb7545b6aa39e17629205a64204
Momme Butenschön <momme.butenschon@cmcc.it>
2018-03-12 13:45:46 +0100

    init_bfm does not carry arguments any longer.

--- src/pom/coupling/pom_ini_bfm_1d.F90
+++ src/pom/coupling/pom_ini_bfm_1d.F90
@@ -218,7 +218,7 @@
 !  ***********************************************************************
 !  ***********************************************************************
 !
-   call init_bfm(namlst)
+   call init_bfm
 !
 !  -----INITIALISE STATE VARIABLE NAMES AND DIAGNOSTICS (FOR NETCDF OUTPUT)-----
 !
