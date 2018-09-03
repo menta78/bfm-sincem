9c9f76c889612b316bd054db6bfdd78bdaad7a63
Tomas Lovato <tomas.lovato@cmcc.it>
2018-09-03 10:41:46 +0200

    Reinstate remineralization rates for each constituents of Benthic Detritus

--- src/BFM/PelBen/BenthicReturn.F90
+++ src/BFM/PelBen/BenthicReturn.F90
@@ -105,35 +105,35 @@
   fact = retfac* et* eo
 
   ! remineralization fluxes
-  rate  =   p_reminQ6* fact* Q6c(:)
+  rate  =   p_rmnQ6c* fact* Q6c(:)
   call flux_vector( iiBen, ppQ6c,ppQ6c,-( rate) )
   jbotO2o(:)  = - rate / MW_C
   jbotO3c(:)  = rate
 
-  rate  =   p_reminQ1* fact* Q1c(:)
+  rate  =   p_rmnQ1c* fact* Q1c(:)
   call flux_vector( iiBen, ppQ1c,ppQ1c,-( rate) )
   jbotO2o(:)  = jbotO2o(:) - rate / MW_C
   jbotO3c(:)  = jbotO3c(:) + rate
 
-  rate  =   p_reminQ6* fact* Q6p(:)
+  rate  =   p_rmnQ6p* fact* Q6p(:)
   call flux_vector( iiBen, ppQ6p,ppQ6p,-( rate) )
   jbotN1p(:)  = rate
 
-  rate  =   p_reminQ1* fact* Q1p(:)
+  rate  =   p_rmnQ1p* fact* Q1p(:)
   call flux_vector( iiBen, ppQ1p,ppQ1p,-( rate) )
   jbotN1p(:)  = jbotN1p(:)+ rate
 
-  rate  =   p_reminQ6* fact* Q6n(:)
+  rate  =   p_rmnQ6n* fact* Q6n(:)
   call flux_vector( iiBen, ppQ6n,ppQ6n,-( rate) )
   jbotN3n(:)  = rate* p_pQIN3
   jbotN4n(:)  = rate*( ONE - p_pQIN3)
 
-  rate  =   p_reminQ1* fact* Q1n(:)
+  rate  =   p_rmnQ1n* fact* Q1n(:)
   call flux_vector( iiBen, ppQ1n,ppQ1n,-( rate) )
   jbotN3n(:)  = jbotN3n(:)+ rate* p_pQIN3
   jbotN4n(:)  = jbotN4n(:)+ rate*( ONE - p_pQIN3)
 
-  rate  =   p_reminQ6* fact* Q6s(:)
+  rate  =   p_rmnQ6s* fact* Q6s(:)
   call flux_vector( iiBen, ppQ6s,ppQ6s,-( rate) )
   jbotN5s(:)  = rate
 
