e391289792d7e592cbe938acd93b8c0ae4482de1
Momme Butenschön <momme.butenschon@cmcc.it>
2019-07-16 11:17:34 +0200

    Added missing parameter declaration for PelBac4.

--- src/BFM/Pel/ModulePelBac.F90
+++ src/BFM/Pel/ModulePelBac.F90
@@ -76,6 +76,7 @@
   ! p_suR3      [1/d]            Specific potential uptake for semi-refractory DOC
   ! p_suR6      [1/d]            Specific potential uptake for POM (1/d)
   ! p_sum       [1/d]            Potential specific growth rate
+  ! p_qsum      [-]              Ratio of first-slope maximum growth (p_sum) to particulate substrate limited maximum growth at inifinite bacteria concentrations
   ! p_pu_ra     [-]              Activity respiration fraction
   ! p_pu_ra_o   [-]              Additional respiration fraction at low O2 conc
   ! p_srs       [1/d]            Specific rest respiration
@@ -103,6 +104,7 @@
   real(RLEN)  :: p_suR6(iiPelBacteria)
   real(RLEN)  :: p_suR3(iiPelBacteria)
   real(RLEN)  :: p_sum(iiPelBacteria)
+  real(RLEN)  :: p_qsum(iiPelBacteria)
   real(RLEN)  :: p_pu_ra(iiPelBacteria)
   real(RLEN)  :: p_pu_ra_o(iiPelBacteria)
   real(RLEN)  :: p_srs(iiPelBacteria)
@@ -134,7 +136,7 @@
   namelist /PelBacteria_parameters/ p_version, p_q10, p_chdo, p_sd, p_sd2, p_suhR1, &
     p_sulR1, p_suR2, p_suR6, p_sum, p_pu_ra, p_pu_ra_o, p_pu_ea_R3, p_srs, &
     p_suR3, p_qpcPBA, p_qlpc, p_qncPBA, p_qlnc, p_qun, p_qup, p_chn, p_chp, &
-    p_ruen, p_ruep, p_rec
+    p_ruen, p_ruep, p_rec, p_qsum
 
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   !BEGIN compute
