4f4777af7fa0abf03be7f7f4b54b067f0fda4389
Tomas Lovato <tomas.lovato@cmcc.it>
2020-06-05 00:34:50 +0200

    mute warnings when copying files form preset to tmp dir (e.g. cannot copy folder)

--- build/bfm_configure.sh
+++ build/bfm_configure.sh
@@ -354,7 +354,7 @@ if [ ${GEN} ]; then
     if [ ! -d ${blddir} ]; then mkdir ${blddir}; fi
     cd ${blddir}
     rm -rf *
-    cp ${presetdir}/* ${blddir}
+    cp ${presetdir}/* ${blddir} 2>/dev/null
     
     #add -D to cppdefs
     cppdefs=`echo ${CPPDEFS} | sed -e 's/\([a-zA-Z_0-9]*\)/-D\1/g'`
