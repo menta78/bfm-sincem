17f35b81dfb6cd4271981e65c9dc510a2695d85d
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2022-09-16 10:36:51 +0200

    Renaming BFM0D -> BFM1D

--- src/ogstm/BFM1D_INIT_IO_CHANNELS.F90
+++ src/ogstm/BFM1D_INIT_IO_CHANNELS.F90
@@ -2,7 +2,7 @@
 ! MODEL  BFM - Biogeochemical Flux Model
 !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 !
-! ROUTINE: BFM0D_NO_BOXES
+! ROUTINE: BFM1D_INIT_IO_CHANNELS
 !
 ! DESCRIPTION
 !   Initializing procedure
@@ -19,46 +19,25 @@
 !   MERCHANTEABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 !   See the GNU General Public License for more details.
 !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
 #include "cppdefs.h"
 !
-subroutine BFM0D_NO_BOXES(N,X,Y,Z,XY)
+subroutine BFM1D_INIT_IO_CHANNELS()
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   ! The following global constants are used: RLEN
 
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   ! Modules (use of ONLY is strongly encouraged!)
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  use global_mem, ONLY:RLEN, bfm_lwp, LOGUNIT
-  use mem, ONLY:NO_BOXES,NO_BOXES_X,NO_BOXES_Y,NO_BOXES_Z,NO_BOXES_XY, NO_STATES, &
-       NO_D3_BOX_STATES
-  use api_bfm, ONLY: SRFindices,BOTindices
+  use global_mem, only:RLEN,LOGUNIT,NMLUNIT,bfm_file_FirstUnit
+  use api_bfm, ONLY: GetLun
 
   IMPLICIT NONE
 
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   ! Local Variables
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  integer, intent(in) :: N,X,Y,Z,XY
-
-
-  NO_BOXES   = N  
-  NO_BOXES_X = X
-  NO_BOXES_Y = Y
-  NO_BOXES_Z = Z
-  NO_BOXES_XY = XY
-  NO_STATES   = NO_D3_BOX_STATES * NO_BOXES
-  LEVEL1 'NO_BOXES_TOT', N
-  LEVEL1 'NO_BOXES_X', X
-  LEVEL1 'NO_BOXES_Y', Y
-  LEVEL1 'NO_BOXES_Z', Z
-  LEVEL1 'NO_BOXES_XY', XY
-  LEVEL1 'allocating NO_BOXES_XY'
-  allocate(SRFindices(NO_BOXES_XY))
-  allocate(BOTindices(NO_BOXES_XY))
   
-  
-  LEVEL1 'NO_STATES', NO_STATES  
-  LEVEL1 'NO_D3_BOX_STATES',NO_D3_BOX_STATES 
+  NMLUNIT=GetLun()+1
 
-end subroutine BFM0D_NO_BOXES
+
+end subroutine BFM1D_INIT_IO_CHANNELS
