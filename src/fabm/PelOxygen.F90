1862ce761ef729dee4701f6ea0256aa411dff37f
Paolo Lazzari <plazzari@noctiluca.ogs.it>
2023-03-31 08:31:45 +0200

    Adding updated copyrights

--- src/fabm/PelOxygen.F90
+++ src/fabm/PelOxygen.F90
@@ -18,16 +18,18 @@
 !
 ! COPYING
 !   
-!   Copyright (C) 2017 BFM System Team (bfm_st@lists.cmcc.it)
+! COPYING
 !
-!   This program is free software; you can redistribute it and/or modify
+!   Copyright (C) 2023 BFM System Team (bfm_st@cmcc.it)
+!
+!   This program is free software: you can redistribute it and/or modify
 !   it under the terms of the GNU General Public License as published by
-!   the Free Software Foundation;
+!   the Free Software Foundation.
 !   This program is distributed in the hope that it will be useful,
 !   but WITHOUT ANY WARRANTY; without even the implied warranty of
-!   MERCHANTEABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!   GNU General Public License for more details.
-!
+!   MERCHANTEABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!   See the GNU General Public License for more details.
+!-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 !EOP
 !-------------------------------------------------------------------------!
 !BOC
