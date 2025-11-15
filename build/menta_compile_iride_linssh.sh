4e1f23ca100fd3260dc33ca9a30bbb573662e1ef
Lorenzo Mentaschi <lm09621@login2.juno.cmcc.scc>
2025-11-15 14:54:08 +0100

    compilation scripts

---
+++ build/menta_compile_iride_linssh.sh
@@ -0,0 +1,5 @@
+export BFMDIR=/work/cmcc/lm09621/src/git/BiogeochemicalFluxModel/
+export NEMODIR=/work/cmcc/lm09621/src/git/nemo/
+. set_env_nemo4.sh
+./bfm_configure.sh -gcd -a X64_JUNO -p IRIDE00_linssh
+
